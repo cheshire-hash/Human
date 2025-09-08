d40195e3fd98cc3b80a50ff8f2186a9848c5ec46
Mandi_pv66 <Mandi_pv66@29-3>
2025-09-08 13:17:50 +0300

    реализация открытой и закрытой части

--- Human/Human.cpp
+++ Human/Human.cpp
@@ -1,1 +1,40 @@
 #include "Human.h"
+
+Human::~Human()
+{
+}
+
+void Human::Init()
+{
+}
+
+void Human::Print()
+{
+}
+
+char* Human::GetName()
+{
+	return nullptr;
+}
+
+char* Human::GetS()
+{
+	return nullptr;
+}
+
+int Human::GetA()
+{
+	return 0;
+}
+
+void Human::SetName(const char* n)
+{
+}
+
+void Human::SetS(const char* s)
+{
+}
+
+void Human::SetA(int a)
+{
+}
--- Human/Human.h
+++ Human/Human.h
@@ -1,5 +1,21 @@
 #pragma once
 class Human
 {
+	char* name;
+	char* surname;
+	int age;
+
+	Human();
+	Human(const char* n, const char* s, const char* a);
+	~Human();
+	void Init();
+	void Print();
+	char* GetName();
+	char* GetS();
+	int GetA();
+
+	void SetName(const char* n);
+	void SetS(const char* s);
+	void SetA(int a);
 };
 
